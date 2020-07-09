221e3806c837cd88719b0245df34c0e3188c3096
heliu <398653669@qq.com>
2020-07-09 23:32:19 +0800

    SlidingMenu2.kt 实现侧滑。过渡效果。

--- app/src/main/java/com/example/kotlintest/SlidingMenu2.kt
+++ app/src/main/java/com/example/kotlintest/SlidingMenu2.kt
@@ -2,6 +2,7 @@ package com.example.kotlintest
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Color
 import android.util.AttributeSet
 import android.util.DisplayMetrics
 import android.util.Log
@@ -10,19 +11,23 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.HorizontalScrollView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.ViewCompat
 
-class SlidingMenu : HorizontalScrollView {
+class SlidingMenu2 : HorizontalScrollView {
     private var mMenuWidth: Int = 0;
-    private lateinit var menuLayout: View
-    private lateinit var homeLayout: View
+    private lateinit var newMenuRoot: ViewGroup
+    private lateinit var newHomeRoot: ViewGroup
     private lateinit var mGestureDetector: GestureDetectorCompat;
     private var TAG = "hltag"
     val MIN_DISTANCE = 100
     val VELOCITY_X = 2000
     var isIntercept: Boolean = false
     private var drawerState: Boolean = false
+
 
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -31,9 +36,9 @@ class SlidingMenu : HorizontalScrollView {
         attrs,
         defStyleAttr
     ) {
-        val typedArray = context!!.obtainStyledAttributes(attrs, R.styleable.SlidingMenu);
+        val typedArray = context!!.obtainStyledAttributes(attrs, R.styleable.SlidingMenu2);
         val marginWidth =
-            typedArray.getDimension(R.styleable.SlidingMenu_marginRightWidth, dp2px(context, 50f))
+            typedArray.getDimension(R.styleable.SlidingMenu2_marginRightWidth2, dp2px(context, 50f))
         mMenuWidth = (getScreenWidth(context) - marginWidth).toInt();
         typedArray.recycle()
         mGestureDetector =
@@ -114,8 +119,8 @@ class SlidingMenu : HorizontalScrollView {
         super.onFinishInflate()
         val container = getChildAt(0) as ViewGroup
 
-        menuLayout = container.getChildAt(0)
-        homeLayout = container.getChildAt(1)
+        val menuLayout = container.getChildAt(0)
+        val homeLayout = container.getChildAt(1)
         val menuLayoutParams = menuLayout.layoutParams;
 
         menuLayoutParams.width = mMenuWidth.toInt()
@@ -123,8 +128,31 @@ class SlidingMenu : HorizontalScrollView {
         val homeLayoutParams = homeLayout.layoutParams;
         homeLayoutParams.width = getScreenWidth(context)
         homeLayout.layoutParams = homeLayoutParams;
+        container.removeAllViews()
 
+        newMenuRoot = RelativeLayout(context)
+        newMenuRoot.layoutParams = ViewGroup.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        val menuShadeView = View(context)
+        menuShadeView.layoutParams = menuLayoutParams
+        menuShadeView.setBackgroundColor(Color.parseColor("#55000000"))
+        newMenuRoot.addView(menuLayout)
+        newMenuRoot.addView(menuShadeView)
 
+        newHomeRoot = RelativeLayout(context)
+        newHomeRoot.layoutParams = homeLayoutParams
+//        val homeShadeView = View(context)
+        val homeShadeView = View(context)
+        homeShadeView.layoutParams = homeLayoutParams
+        homeShadeView.setBackgroundColor(Color.parseColor("#55000000"))
+        homeShadeView.alpha = 0f
+        newHomeRoot.addView(homeLayout)
+        newHomeRoot.addView(homeShadeView)
+
+        container.addView(newMenuRoot)
+        container.addView(newHomeRoot)
         //初始化进来是关闭的
 //        scrollTo(mMenuWidth,0)
 
@@ -158,7 +186,7 @@ class SlidingMenu : HorizontalScrollView {
         isIntercept = false;
         if (drawerState) {
             val x = ev.x;
-            if (x > menuLayout.measuredWidth) {
+            if (x > newMenuRoot.measuredWidth) {
                 Log.i(TAG, "intercept homeLayout")
                 closeMenu()
                 isIntercept = true
@@ -175,30 +203,33 @@ class SlidingMenu : HorizontalScrollView {
 
     override fun onScrollChanged(l: Int, t: Int, oldl: Int, oldt: Int) {
         super.onScrollChanged(l, t, oldl, oldt)
-//        Log.i("hltag", "left:$l top:$t    oldl${oldl}   oldt${oldt}")
+        Log.i("hltag", "left:$l top:$t    oldl${oldl}   oldt${oldt}")
         var scale = 1f * l / mMenuWidth
-        val rightScale = 0.7f + 0.3f * scale
+//        val rightScale = 0.7f + 0.3f * scale
 
 
         //右边缩放
-        homeLayout.pivotX = 0f
-        homeLayout.pivotY = (measuredHeight / 2).toFloat()
-        homeLayout.scaleX = rightScale
-        homeLayout.scaleY = rightScale
+//        homeLayout.pivotX = 0f
+//        homeLayout.pivotY = (measuredHeight / 2).toFloat()
+//        homeLayout.scaleX = rightScale
+//        homeLayout.scaleY = rightScale
 
 
-        //左边缩放
-        val leftScale = 0.7f + 0.3f * (1 - scale)
-        menuLayout.pivotX = measuredWidth.toFloat()
-        menuLayout.pivotY = (measuredHeight / 2).toFloat()
-        menuLayout.scaleX = leftScale
-        menuLayout.scaleY = leftScale
         //左边alpha
-        val leftAlpha = 0.5f + 0.5f * (1 - scale)
-        menuLayout.alpha = leftAlpha
+//        val leftAlpha = 0.5f + 0.5f * (1 - scale)
+//        menuLayout.alpha = leftAlpha
+//
+//
+//        //右边
+        val rightAlpha = 1 - scale
+        newHomeRoot.getChildAt(1).alpha = rightAlpha
+        //左边alpha
+        val leftAlpha = scale
+        newMenuRoot.getChildAt(1).alpha = leftAlpha
+
 
         //左边平移
-        menuLayout.setTranslationX(0.2f * l)
+        newMenuRoot.setTranslationX(0.2f * l)
 
     }
 
